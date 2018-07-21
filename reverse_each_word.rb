2c3180ca6cd85eade08b7b6a9d41eeda0b72d18d
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 21:05:28 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array_from_s = string.split (" ")
-  array_from_s.collect do |w|
-    w.reverse!
-  end
+  reversed_arr = array_from_s.collect do |w|
+                 w.reverse!
+                 end
 end 
